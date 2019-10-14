f6b96a5fadbd780125e2fe1ba43e42ff00395611
megabogo <devbogo@gmail.com>
2019-10-14 15:45:29 +0900

    d

--- lib/main.dart
+++ lib/main.dart
@@ -37,35 +37,49 @@ class LottoBoardState extends State<LottoBoard> {
     final int min = 1;
     final int max = 45;
 
-    var lottoNumbers = Set();
+    var lottoNumbers = new List();
+    var nums = Set();
 
     // 로또 번호 생성 
     var rng = new Random();
     void addLottoNumber() {
+      resetLottoNumber();
       while (true) {
-        var num = rng.nextInt((min - min +1) + min);
-        if(lottoNumbers.add(num)) {
-          if(lottoNumbers.length == 6) {
+        var num = rng.nextInt((max - min +1) + min).toString();
+        if(nums.add(num)) {
+          if(nums.length == 6) {
             break;   
           }
         }        
       }
+      lottoNumbers = getLottoNumber();
     }
 
     void resetLottoNumber() {
       lottoNumbers.clear();
+      nums.clear();
     }
     
+    List<Text> getLottoNumber() {
+      return nums.map((text) => Text(text, style: TextStyle(color: Colors.blue),)).toList();
+      //return lottoNumbers.map((text) => Text(text, style: TextStyle(color: Colors.blue),)).toList();
+    }
+
     @override
     Widget build(BuildContext context) {
         return Column(
             children: <Widget>[
-                Container(
-                    child: Text(
-                        lottoNumbers.toString().replaceAll('/{}/', ''),
-                        style: TextStyle(fontSize: 128),
-                    ),
-                    padding: EdgeInsets.all(32),
+                Expanded(    
+                  child: new ListView.builder(
+                    itemCount: lottoNumbers.length,
+                    itemBuilder: (BuildContext ctxt, int index) {
+                      //return new Text(lottoNumbers);
+                      return lottoNumbers[index];
+                    }
+                  )
+                ),
+                Column(
+                  
                 ),
                 Row(
                     children: <Widget>[
